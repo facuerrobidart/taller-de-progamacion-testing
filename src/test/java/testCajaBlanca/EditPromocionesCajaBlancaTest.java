de1ade88191f63b8eec4ea0d411909ed45ee1448
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2022-11-15 18:56:25 -0300

    testing caja blanca editar promociones

--- src/test/java/testCajaBlanca/EditPromocionesCajaBlancaTest.java
+++ src/test/java/testCajaBlanca/EditPromocionesCajaBlancaTest.java
@@ -1,4 +1,97 @@
 package testCajaBlanca;
 
+import dto.PromocionProductoDTO;
+import enums.Dias;
+import modelo.Empresa;
+import modelo.promociones.PromocionFija;
+import negocio.GestionDePromociones;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class EditPromocionesCajaBlancaTest {
+    private EscenariosEditPromociones escenarios = new EscenariosEditPromociones();
+    private GestionDePromociones g = GestionDePromociones.get();
+    private Empresa empresa = Empresa.getEmpresa();
+
+    @Test
+    public void testEditPromocionExito() {
+        escenarios.cargarListaPromociones();
+        PromocionFija promo = escenarios.getPromo1();
+        String nombre = promo.getNombre();
+
+        promo.setDtoPorCantPrecioU(80.0);
+
+        g.modificaPromocionFija(
+            new PromocionProductoDTO(
+                promo.getNombre(),
+                promo.isActivo(),
+                promo.getDiasPromo(),
+                promo.getProducto(),
+                promo.isDosPorUno(),
+                promo.isDtoPorCant(),
+                promo.getDtoPorCantMin(),
+                promo.getDtoPorCantPrecioU()
+        ));
+
+        Assert.assertTrue(
+                empresa.getPromocionesFijas()
+                .stream()
+                .anyMatch(p -> p.getNombre().equalsIgnoreCase(nombre) && p.getDtoPorCantPrecioU() == 80.0)
+        );
+        //me fijo que la promo se haya modificado
+
+        escenarios.removerListaPromociones();
+    }
+
+    @Test
+    public void testEditPromocionListaVacia() {
+        escenarios.cargarListaVacia();
+
+        g.modificaPromocionFija(
+                new PromocionProductoDTO(
+                        "Promo test",
+                        true,
+                        List.of(Dias.DOMINGO,Dias.JUEVES),
+                        escenarios.getProd1(),
+                        true,
+                        false,
+                        0,
+                        0.0
+                ));
+
+        Assert.assertTrue(
+                empresa.getPromocionesFijas()
+                        .stream()
+                        .noneMatch(p -> p.getNombre().equalsIgnoreCase("Promo test"))
+        ); //me fijo que la promo no se haya agregado
+
+        escenarios.restaurarListaPromociones();
+    }
+
+    @Test
+    public void testEditPromocionInexistente() {
+        escenarios.cargarListaPromociones();
+
+        g.modificaPromocionFija(
+            new PromocionProductoDTO(
+                "Promo inexistente",
+                true,
+                List.of(Dias.DOMINGO,Dias.JUEVES),
+                escenarios.getProd1(),
+                true,
+                false,
+                0,
+                0.0
+        ));
+
+        Assert.assertTrue(
+                empresa.getPromocionesFijas()
+                .stream()
+                .noneMatch(p -> p.getNombre().equalsIgnoreCase("Promo inexistente"))
+        ); //me fijo que la promo no se haya agregado
+
+        escenarios.removerListaPromociones();
+    }
 }
--- src/test/java/testCajaBlanca/EscenariosEditPromociones.java
+++ src/test/java/testCajaBlanca/EscenariosEditPromociones.java
@@ -3,19 +3,29 @@ package testCajaBlanca;
 import enums.Dias;
 import modelo.Empresa;
 import modelo.Producto;
+import modelo.promociones.Promocion;
 import modelo.promociones.PromocionFija;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EscenariosEditPromociones {
     private Empresa empresa;
+    private Producto prod1;
+    private Producto prod2;
+    private PromocionFija promo1;
+    private PromocionFija promo2;
+    private PromocionFija promo3;
+    private PromocionFija promo4;
+    Set<PromocionFija> promocionesFijas = new HashSet<>();
 
 
-    public void cargarListaProductos(){
+    public void cargarListaPromociones() {
         empresa = Empresa.getEmpresa();
-        Producto prod1 = new Producto("Coca Cola", 100, 75, 20);
+        prod1 = new Producto("Coca Cola", 100, 75, 20);
         empresa.getProductos().add(prod1);
-        Producto prod2 = new Producto("Pepsi", 100, 75, 20);
+        prod2 = new Producto("Pepsi", 100, 75, 20);
         empresa.getProductos().add(prod2);
 
         PromocionFija promo1 = new PromocionFija("Promo 1 Coca", List.of(Dias.LUNES,Dias.MARTES), prod1, true, false, 0, 0.0);
@@ -24,7 +34,34 @@ public class EscenariosEditPromociones {
         PromocionFija promo4 = new PromocionFija("Promo 2 Pepsi", List.of(Dias.LUNES,Dias.MARTES), prod2, false, true, 3, 10.0);
 
         empresa.getPromocionesFijas().addAll(List.of(promo1, promo2, promo3, promo4));
+    }
 
+    public void cargarListaVacia() {
+        promocionesFijas = empresa.getPromocionesFijas();
+        empresa.setPromocionesFijas(new HashSet<>());
+    }
+
+    public void removerListaPromociones() {
+        empresa.getProductos().remove(prod1);
+        empresa.getProductos().remove(prod2);
+        empresa.getPromocionesFijas().remove(promo1);
+        empresa.getPromocionesFijas().remove(promo2);
+        empresa.getPromocionesFijas().remove(promo3);
+        empresa.getPromocionesFijas().remove(promo4);
+    }
+
+    public void restaurarListaPromociones() {
+        empresa.getProductos().add(prod1);
+        empresa.getProductos().add(prod2);
+        empresa.getPromocionesFijas().addAll(promocionesFijas);
+    }
+
+    public PromocionFija getPromo1() {
+        return promo1;
+    }
+
+    public Producto getProd1() {
+        return prod1;
     }
 
 }
